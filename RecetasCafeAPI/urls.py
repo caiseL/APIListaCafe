375633ba533b9de92081e5c8cc56231848d2778a
caisel <cristophertraperoo@gmail.com>
2021-05-04 20:50:49 -0600

    Removidos imports innecesarios

--- RecetasCafeAPI/urls.py
+++ RecetasCafeAPI/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from recetas.views import ListaRecetas, api_root, CrearReceta
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', api_root),
