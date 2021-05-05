b2d8344f6af7ecfacfb85f2c6190babb2e6ff9b7
caisel <cristophertraperoo@gmail.com>
2021-05-04 22:17:25 -0600

    Removida linea de whitenoise

--- RecetasCafeAPI/settings.py
+++ RecetasCafeAPI/settings.py
@@ -126,7 +126,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
